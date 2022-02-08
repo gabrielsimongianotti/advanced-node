8719d1c86c8bfe3b630338785b617746b1330ec4
gabrielsimongianotti <gabrielgianotti5@gmail.com>
2022-02-08 00:17:58 -0300

    refactor: changed the way to connect with the postgres

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,11 +1,10 @@
 import './config/module-alias'
 import { app } from '@/main/config/app'
 import { env } from './config/env'
-import { config } from '@/infra/postgres/helper'
 
 import 'reflect-metadata'
 import { createConnection } from 'typeorm'
 
-createConnection(config)
+createConnection()
   .then(() => app.listen(env.appPort, () => console.log(`Server sk running at http://localhost:${env.appPort}`)))
   .catch(console.error)
