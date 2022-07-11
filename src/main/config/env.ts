e1f5a05de3fd0d4b45425254f7d1b42dbf8be5b4
Gatão <gabrielgianotti5@gmail.com>
2022-07-11 01:11:38 -0300

    fix: update FB_CLIENT_ID and FB_CLIENT_SECRET

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,8 +1,8 @@
-export const env = {
+export const env = {// fis isso para facilitar os teste de vcs
   facebookApi: {
-    clientId: process.env.FB_CLIENT_ID ?? '650961116275251',
+    clientId: process.env.FB_CLIENT_ID ?? '559580479142130',
     clientSecret:
-      process.env.FB_CLIENT_SECRET ?? '05460bbc1e59eb7044764f343da0ba22'
+      process.env.FB_CLIENT_SECRET ?? '42f0721c774723e94d9cd844fcc075f1'
   },
   appPort: process.env.PORT ?? '8081',
   jwtSecret: process.env.JWT_SECRET ?? 'FGSDGSDFGSDFGSDGFSD'
